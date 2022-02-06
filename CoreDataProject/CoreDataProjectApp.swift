2e5a25e3ec8984aa5d061c8f28052345c1202997
Dom Bryan <dombguitar@gmail.com>
2022-02-06 15:47:34 +0000

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Dom Bryan on 06/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
