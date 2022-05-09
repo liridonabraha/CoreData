7206c59b7525baf918c77276b3342e03a1ac9c29
Liridona <donabraha94@gmail.com>
2022-05-09 10:54:27 +0100

    Project 12

---
+++ CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Dom Bryan on 10/02/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
